9249d96a44dfcd139f0aabb607fd635d3e968066
Emanuel Keiner <eekeiner@gmail.com>
2021-06-15 23:25:08 -0300

    minuto 23 del vidio

--- m3 rutas y controladores/app.js
+++ m3 rutas y controladores/app.js
@@ -22,6 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+ app.use('/prueba', function(req, res){
+   res.send('hola soy la pagina de prueba')
+ }); //cierra linea 25
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
