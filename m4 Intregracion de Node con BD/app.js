7b2bda95250042531f3c928c3f9bef420a7a0a7f
Emanuel Keiner <eekeiner@gmail.com>
2021-07-19 22:29:22 -0300

    terminado

--- m4 Intregracion de Node con BD/app.js
+++ m4 Intregracion de Node con BD/app.js
@@ -9,6 +9,7 @@ var pool = require('./models/bd')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { PayloadTooLarge } = require('http-errors');
 
 var app = express();
 
@@ -25,10 +26,40 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-pool.query('SELECT * FROM empleados').then(function
+/* pool.query('SELECT * FROM empleados').then(function   PARA ACCEDER A REGISTROS
   (resultados){
     console.log(resultados)
-  });
+  }); */
+
+ /*  var obj = {
+    nombre: 'Juan',
+    apellido: 'Lopez',
+    trabajo: 'docente',
+    edad: 38,
+    salario: 87000,
+    mail: 'juanlopez@bignet.com'
+  }
+
+  pool.query('INSERT INTO empleados SET ?', [obj]).then(function(resultados){
+    console.log(resultados)
+  })
+ */
+
+var id = 1;          
+var obj = {
+  nombre: 'Pablo',
+  apellido: 'Gomez'
+}
+
+pool.query('UPDATE empleados SET ? WHERE id_emp=?', [obj, id]).then(function (resultados){
+  console.log(resultados);
+});
+
+/* var id = 24;
+pool.query('DELETE FROM empleados WHERE id_emp=?', [id]).then(function (resultados){
+  console.log(resultados);
+});
+ */
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
