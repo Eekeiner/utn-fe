ffd4debcc224c0fe84a41efd4381b4607bc9a5af
Emanuel Keiner <eekeiner@gmail.com>
2021-08-19 23:26:35 -0300

    terminado

--- m4 Integracion de Node con BD pt 2/app.js
+++ m4 Integracion de Node con BD pt 2/app.js
@@ -5,12 +5,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var contactoRouter = require('./routes/contacto')
 var loginRouter = require('./routes/admin/login'); //admin/login.js
-
+var adminRouter = require('./routes/admin/novedades')
 var app = express();
 
 // view engine setup
@@ -24,10 +25,30 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('public'))
 
+app.use(session({
+  secret: 'ñl2k340987ncq0790fas90df8',
+  resave: false,
+  saveUninitialized: true
+}))
+
+secured = async (req, res, next) =>{
+  try {
+    console.log(req.session.id_usuario);
+    if (req.session.id_usuario){
+      next();
+    } else {
+      res.redirect('/admin/login')
+    }
+  } catch (error) {
+    console.log(error)
+}
+}
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/contacto', contactoRouter);
 app.use('/admin/login', loginRouter);
+app.use('/admin/novedades', secured, adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
