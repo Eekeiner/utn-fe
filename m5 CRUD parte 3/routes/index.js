52b9ef7e02729586180f800bff8d529875840dc4
Emanuel Keiner <eekeiner@gmail.com>
2021-08-22 17:56:52 -0300

    avanzando

---
+++ m5 CRUD parte 3/routes/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+const { get } = require('http');
+var router = express.Router();
+var nodemailer = require("nodemailer");
+const { getMaxListeners } = require('process');
+var novedadesModel = require('../models/novedadesModel');
+
+
+/* GET home page. */
+router.get('/', async function (req, res, next) {
+  var novedades = await novedadesModel.getNovedades();
+  res.render('index',{
+    novedades
+  });
+});
+
+router.post('/', async (req, res, next) => {
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
+  var email = req.body.email;
+  var direccion = req.body.direccion;
+  var ciudad = req.body.ciudad;
+  var provincia = req.body.provincia;
+  var codpostal = req.body.codpostal;
+
+  var obj = {
+    to: 'eekeiner@gmail.com',
+    subject: 'Contacto desde la web',
+    html: "La persona " + nombre + " de apellido " + apellido + " se contacto a traves de la web de la pagina de la optica y pide que le envien mas info a esta direccion de email " + email + ". <br> Su direccion es " + direccion + " en la ciudad de " + ciudad + " de la provincia de " + provincia + ". Codigo postal " + codpostal
+  } // cierra var de objeto
+
+  var transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  }); //cierra trasporter
+
+  var info = await transport.sendMail(obj);
+
+  res.render('index', {
+    message: 'Mensaje enviado correctamente',
+  });
+});
+module.exports = router;
